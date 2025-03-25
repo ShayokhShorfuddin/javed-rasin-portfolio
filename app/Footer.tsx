bb634d3c880f14fda9ee7a32028f1f9b3245661a
Shayokh <shayokhshuvro14@gmail.com>
2025-03-25 12:28:41 +0600

    add facebook contact

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -114,7 +114,8 @@ export default function Footer() {
 								</AlertDialogDescription>
 							</AlertDialogHeader>
 
-							<div className="flex flex-col gap-y-5 text-neutral-800">
+							<div className="flex flex-col gap-y-4 text-neutral-800">
+								<ContactFacebook />
 								<ContactPhone />
 								<ContactEmail />
 								<ContactAddress />
@@ -128,6 +129,32 @@ export default function Footer() {
 				</div>
 			</div>
 		</footer>
+	);
+}
+
+export function ContactFacebook() {
+	return (
+		<Link
+			href="https://www.facebook.com/m.javed.rasin"
+			target="_blank"
+			rel="noopener noreferrer"
+			aria-label="Visit SED Bangladesh Facebook page"
+			className="hover:opacity-80 transition-opacity"
+		>
+			<div className="flex gap-x-4">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 512 512"
+					fill="black"
+					className="size-6"
+				>
+					<title>Facebook</title>
+					<path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z" />
+				</svg>
+
+				<p className="text-sm">Javed Rasin (জাবেদ রাসিন)</p>
+			</div>
+		</Link>
 	);
 }
 
