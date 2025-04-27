ef03a102b8b485d83e738db625ff331b8a26d90a
Shayokh <shayokhshuvro14@gmail.com>
2025-04-27 21:31:55 +0600

    disable admin page indexing by crawlers

--- app/admin/[[...index]]/page.tsx
+++ app/admin/[[...index]]/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 import config from "@/sanity.config";
+import type { Metadata } from "next";
 import { NextStudio } from "next-sanity/studio";
+
+export const metadata: Metadata = {
+	robots: {
+		index: false,
+		follow: false,
+	},
+};
 
 export default function AdminPage() {
 	return <NextStudio config={config} />;
